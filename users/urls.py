ea833ed90bb6fa252ef20109552d08eb2fa56a33
Jhos10 <jhostinposada7@gmail.com>
2025-05-11 21:46:55 -0500

    Se hizo la vista de registrar usuario

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView,LogoutView
-
+from .views import RegisterUserView
 urlpatterns = [
+    path('users_register/', RegisterUserView.as_view(), name='register_user'),
     path('users_login/', LoginView.as_view(template_name = "users/login.html"), name='login_user'),
     path('users_logout/', LogoutView.as_view(), name='logout')
 ]
